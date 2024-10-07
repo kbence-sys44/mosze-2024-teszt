7014e8aa48196089b94409458558df9ae846751a
kbence <kadarbence2004@gmail.com>
2024-10-07 21:03:53 +0200

    hibak javitasa

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int* b = new int[NELEMENTS]; // konstans el van irva
-    std::cout << '1-100 ertekek duplazasa' // hianyzo endl
-        for (int i = 0;) // befejezetlen for ciklus
+    int* b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+        for (int i = 0; i<N_ELEMENTS;i++)
         {
             b[i] = i * 2;
         }
-    for (int i = 0; i; i++) // befejezetlen for ciklus
+    for (int i = 0; i<N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" // lezaratlan ln es nem ir ki erteket
+        std::cout << "Ertek:" << b[i] << std::endl;
     }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; // nem float es nincs inicializalva
-    for (int i = 0; i < N_ELEMENTS, i++) // elirt for ciklus
+    float atlag = 0.0f;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i] // hianyzo ;
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS; 
     std::cout << "Atlag: " << atlag << std::endl;
